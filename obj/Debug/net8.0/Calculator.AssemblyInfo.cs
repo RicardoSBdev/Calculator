8c0497b079f2f28aeb5f5e074295e1aceb9136cd
Ricardo Bueno <ricardo.sb.contato@gmail.com>
2025-09-03 18:43:34 -0300

    Adiciona .gitignore e limpa arquivos de build

--- obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37bf92253878c1dd3b6874e00ed77c7c4508e224")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
